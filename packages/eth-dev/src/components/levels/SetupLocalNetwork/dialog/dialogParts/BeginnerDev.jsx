4419397774b861e7d9ab8e5afdae57af32da225e
selimerunkut <se@cypherx.tech>
2023-06-06 22:13:10 +0200

    text corrections

--- packages/eth-dev/src/components/levels/SetupLocalNetwork/dialog/dialogParts/BeginnerDev.jsx
+++ packages/eth-dev/src/components/levels/SetupLocalNetwork/dialog/dialogParts/BeginnerDev.jsx
@@ -10,17 +10,14 @@ export const DIALOG_PART_ID = `${LEVEL_ID}/BeginnerDev`
 const _dialog = [
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>Not to worry, not to worry!</SpeakerLeft>
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">Not to worry, not to worry!</SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-        We’ve recently gathered other Attestors. 
-        
-        I’ll introduce you to the schemas and attestations.
-
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        We’ve recently gathered other Attestors. I’ll introduce you to the schemas and attestations.
         Click Connect Wallet!
       </SpeakerLeft>
     ),
@@ -29,16 +26,15 @@ const _dialog = [
 
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
         You can search for a schema or explore a few featured schemas.
-        
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
         We will choose an existing one to make things easier for you!
       </SpeakerLeft>
     ),
@@ -46,89 +42,99 @@ const _dialog = [
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          Check the list for "Make a Statement" and clixk on it, if you don't find it you can also search for it.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        Check the list for "Make a Statement" and clixk on it, if you don't find it you can also
+        search for it.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          Click on "Attest with Schema"
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">Click on "Attest with Schema"</SpeakerLeft>
+    ),
+    choices: null
+  },
+  {
+    dialog: () => (
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        You can choose an optional recipient if you want to sent your attestations.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          You can choose an optional recipient if you want to sent your attestations.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        You need to provide a message like: "I am a crypto OG"
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          You need to provide a message like: "I am a crypto OG"
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        Now you have the option to leave your attestations onchain for all eternity or you can
+        choose to make them offline and private.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          Now you have the option to leave your attestations onchain for all eternity 
-          or you can choose to make them offline and private.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        If you choose offchain you will just sign the attestations with your wallet and don't pay
+        anything.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          If you choose offchain you will just sign the attestations with your wallet and don't pay anything.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        If you are rich enough to write data onchain, you can pay a fee to store the attestations on
+        the blockchain.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          If you are rich enough to write data onchain, you can pay a fee to store the attestations on the blockchain.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        If you haven't already connect your wallet to the website
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          We go with offchain for now, so click on "Sign and Send" and confirm the transaction in your wallet.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        We go with offchain for now, so click on "Sign and Send" and confirm the transaction in your
+        wallet.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          After you sign your attestations, the details are displayed in a new page.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        After you sign your attestations, the details are displayed in a new page.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          You have also the option to publish it to IPFS or download your attestation.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        You have also the option to publish it to IPFS or download your attestation.
       </SpeakerLeft>
     ),
     choices: null
   },
   {
     dialog: () => (
-      <SpeakerLeft pathToAvatar='./assets/punk_anon.png'>
-          Well done my friend. Be the gas price allways on your side.
+      <SpeakerLeft pathToAvatar="./assets/punk_anon.png">
+        Well done my friend. Be your gas prices allways low.
       </SpeakerLeft>
     ),
     choices: null
